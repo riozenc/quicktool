23c46a14b79c97e6e787b935155e469c10bf0c7c
riozenc <riozenc@DESKTOP-US6TTP8>
2017-06-15 17:45:16 +0800

    讲对象转换成xml 字符串

--- src/main/java/com/riozenc/quicktool/common/util/xml/XmlUtils.java
+++ src/main/java/com/riozenc/quicktool/common/util/xml/XmlUtils.java
@@ -5,30 +5,39 @@
 **/
 package com.riozenc.quicktool.common.util.xml;
 
+import java.lang.reflect.Field;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
+import com.riozenc.quicktool.common.util.reflect.ReflectUtil;
+
 public class XmlUtils {
 
-	public static String string2xml() {
-
+	public static String object2xml(Object object) {
+		if (object == null)
+			throw new NullPointerException("参数不能为null...");
+		Field[] fields = object.getClass().getDeclaredFields();
+		String className = object.getClass().getSimpleName();
+		if (fields == null || fields.length == 0)
+			throw new RuntimeException(object.getClass() + "没有属性...");
 		Document document = DocumentHelper.createDocument();
-		Element rootElement = document.addElement("employee");
-		Element empName = rootElement.addElement("empname");
-		empName.setText("@残缺的孤独");
-		Element empAge = rootElement.addElement("age");
-		empAge.setText("25");
-		Element empTitle = rootElement.addElement("title");
-		empTitle.setText("软件开发工程师");
-		
-		System.out.println(document.asXML());
-		System.out.println(document.asXML().getBytes());
-		System.out.println(new String(document.asXML().getBytes()));
-		return null;
+		Element rootElement = document.addElement(className);
+		for (Field field : fields) {
+			Object value = ReflectUtil.getFieldValue(object, field.getName());
+			if (value != null) {
+				Element element = rootElement.addElement(field.getName());
+				try {
+					element.setText((String) ReflectUtil.typeFormat(String.class, value));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					element.setText((String) value);
+				}
+			}
+		}
+		return document.getRootElement().asXML();
 	}
-	
-	public static void main(String[] args) {
-		string2xml();
-	}
+
 }
